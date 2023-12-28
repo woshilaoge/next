e0d588e5d22aa98b17b2a02009adf9904c51fabd
暁仙 <xiaoxian.xlf@alibaba-inc.com>
2023-12-28 12:11:23 +0800

    chore: 调整types

--- components/dropdown/__tests__/a11y-spec.tsx
+++ components/dropdown/__tests__/a11y-spec.tsx
@@ -4,11 +4,9 @@ import '../style';
 import { test, createContainer, testReact } from '../../util/__tests__/a11y/validate';
 
 const portalContainerId = 'a11y-portal-id';
-let portalContainer: HTMLElement;
-
 describe('Dropdown A11y', () => {
     it('should not have any violations', async () => {
-        portalContainer = createContainer(portalContainerId);
+      const portalContainer: HTMLElement = createContainer(portalContainerId);
         await testReact(
             // eslint-disable-next-line jsx-a11y/anchor-is-valid
             <Dropdown trigger={<a>Hello dropdown</a>} visible container={portalContainer}>
